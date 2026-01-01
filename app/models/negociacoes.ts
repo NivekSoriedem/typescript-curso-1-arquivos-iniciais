be9c13d5a391fe000b87e0777deeef6d20dd146c
Kevin <kevinmedeiroliver21@gmail.com>
2026-01-01 17:11:25 -0300

    ADD : novo script para lidaas com adicao e listagem das negociações criadas.

---
+++ app/models/negociacoes.ts
@@ -0,0 +1,19 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+  private negociacoes : Array<Negociacao> = [];
+
+  adiciona(negociacao : Negociacao) {
+    this.negociacoes.push(negociacao);
+  }
+
+  lista() : ReadonlyArray<Negociacao> {
+    return this.negociacoes;
+  }
+}
+
+const negociacoes = new Negociacoes();
+
+negociacoes.lista().forEach(n => {
+  console.log(n)
+})
